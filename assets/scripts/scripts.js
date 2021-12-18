7948e48fc96c4e65ff4480031335b40c3bf2c3ed
Katie Sexton <ksexton@adobe.com>
2021-12-17 21:59:19 -0700

    set basic styles

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -407,7 +407,7 @@ async function loadLazy(doc) {
   const main = doc.querySelector('main');
 
   loadBlocks(main);
-  loadCSS('/styles/lazy-styles.css');
+  loadCSS('/assets/styles/lazy-styles.css');
 }
 
 /**
