71e9f160c79c2f69c84ffdff83dc1858080b6c60
Katie Sexton <ksexton@adobe.com>
2022-07-07 13:56:39 -0600

    fix: footer bug fixes

--- assets/blocks/footer/footer.js
+++ assets/blocks/footer/footer.js
@@ -4,7 +4,6 @@ import { readBlockConfig, decorateIcons } from '../../scripts/scripts.js';
  * loads and decorates the footer
  * @param {Element} block The header block element
  */
-
 export default async function decorate(block) {
   const cfg = readBlockConfig(block);
   block.textContent = '';
