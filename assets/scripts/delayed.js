ddd0daa348a1c230da5211793a17174c56413434
Katie Sexton <ksexton@adobe.com>
2022-07-07 20:32:52 -0600

    feat: externalize links

--- assets/scripts/delayed.js
+++ assets/scripts/delayed.js
@@ -5,3 +5,19 @@ import { sampleRUM } from './scripts.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+function externalizeLinks() {
+  document.querySelectorAll('main a[href]').forEach((a) => {
+    try {
+      const host = new URL(a.href)?.host;
+      if (window.location.host !== host) {
+        a.setAttribute('target', '_blank');
+        a.setAttribute('rel', 'noreferrer');
+      }
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.warn(`Could not externalize ${a.href}`, error);
+    }
+  });
+}
+
+externalizeLinks();
