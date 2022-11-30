b029ebb287cd084920aaeef43b64eedfa35a7008
Katie Sexton <ksexton@adobe.com>
2022-11-29 20:10:42 -0700

    feat: columns block

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,4 +1,11 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+  [...block.children].forEach((row) => {
+    const picture = row.querySelector('picture');
+    if (picture) {
+      const pictureCol = picture.closest('div');
+      if (pictureCol && pictureCol.textContent.trim() === '') pictureCol.className = 'columns-img-col';
+    }
+  });
 }
