5766ffbb5af0898f50b5438b86e830c4da176722
Katie Sexton <ksexton@adobe.com>
2022-12-02 16:51:10 -0700

    feat: menu template

---
+++ templates/menu/menu.js
@@ -0,0 +1,8 @@
+/**
+ * decorates the body with the menu template
+ * @param {Element} body The document body element
+ */
+export default async function decorate(body) {
+  // const main = body.querySelector('main');
+  // setup square functionality
+}
