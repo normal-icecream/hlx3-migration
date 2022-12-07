e8220f996a5b352e56745baa1593c6ae3e7326c7
Katie Sexton <ksexton@adobe.com>
2022-12-07 13:59:23 -0700

    fix: standardize imgs

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,10 +2,11 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
   [...block.children].forEach((row) => {
-    const picture = row.querySelector('picture');
-    if (picture) {
+    const pictures = row.querySelectorAll('picture');
+    pictures.forEach((picture) => {
       const pictureCol = picture.closest('div');
       if (pictureCol && pictureCol.textContent.trim() === '') pictureCol.className = 'columns-img-col';
-    }
+      else pictureCol.className = 'columns-inline-col';
+    });
   });
 }
